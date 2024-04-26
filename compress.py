fix compression factor calculation and add separate QUALITY_FACTOR for UV channels

compress.py
@@ -23,26 +23,45 @@ def idct2(f):
     return idct(idct(f, axis=0 , norm='ortho'), axis=1 , norm='ortho')
 
 BLOCK_SIZE = 8
-QUALITY_FACTOR = 2
-QUANTIZATION_BITS_Y = 4
+QUALITY_FACTOR_Y = 3
+QUALITY_FACTOR_UV = 2
+QUANTIZATION_BITS_Y = 5
 QUANTIZATION_BITS_UV = 4
 
-mask = np.zeros((BLOCK_SIZE, BLOCK_SIZE))
-mask[0,0] = 1
-if QUALITY_FACTOR >= 2:
-    mask[1,0] = 1
-    mask[0,1] = 1
-elif QUALITY_FACTOR >= 3:
-    mask[1,1] = 1
-    mask[0,2] = 1
-    mask[1,2] = 1
-    mask[2,0] = 1
-    mask[2,1] = 1
-print(f'using {np.count_nonzero(mask)}/{BLOCK_SIZE * BLOCK_SIZE} DCT coefficients')
+mask_y = np.zeros((BLOCK_SIZE, BLOCK_SIZE))
+mask_y[0,0] = 1
+if QUALITY_FACTOR_Y >= 2:
+    mask_y[1,0] = 1
+    mask_y[0,1] = 1
+if QUALITY_FACTOR_Y >= 3:
+    mask_y[1,1] = 1
+    mask_y[0,2] = 1
+    mask_y[1,2] = 1
+    mask_y[2,0] = 1
+    mask_y[2,1] = 1
+print(f'using {np.count_nonzero(mask_y)}/{BLOCK_SIZE * BLOCK_SIZE} DCT coefficients for Y')
+
+mask_uv = np.zeros((BLOCK_SIZE, BLOCK_SIZE))
+mask_uv[0,0] = 1
+if QUALITY_FACTOR_UV >= 2:
+    mask_uv[1,0] = 1
+    mask_uv[0,1] = 1
+if QUALITY_FACTOR_UV >= 3:
+    mask_uv[1,1] = 1
+    mask_uv[0,2] = 1
+    mask_uv[1,2] = 1
+    mask_uv[2,0] = 1
+    mask_uv[2,1] = 1
+print(f'using {np.count_nonzero(mask_uv)}/{BLOCK_SIZE * BLOCK_SIZE} DCT coefficients for UV')
+
 
 # Function to apply mask on DCT coefficients
-def apply_mask(dct_matrix):
-    return dct_matrix * mask
+def apply_mask_y(dct_matrix):
+    return dct_matrix * mask_y
+
+# Function to apply mask on DCT coefficients
+def apply_mask_uv(dct_matrix):
+    return dct_matrix * mask_uv
 
 # Load TIFF image
 image = Image.open("lena_color.tif")
@@ -79,11 +98,12 @@ for y in range(0, y_channel.shape[0], BLOCK_SIZE):
 # Perform Inverse DCT on each 8x8 block of the Y, U, and V channels
 for y in range(0, y_channel.shape[0] // BLOCK_SIZE):
     for x in range(0, y_channel.shape[1] // BLOCK_SIZE):
-        y_channel[y * BLOCK_SIZE : (y+1) * BLOCK_SIZE, x * BLOCK_SIZE : (x+1) * BLOCK_SIZE] = idct2(apply_mask(blocks[:, :, y, x, 0]) * (MAX_Y * BLOCK_SIZE) / MAX_QUANT_Y)
-        u_channel[y * BLOCK_SIZE : (y+1) * BLOCK_SIZE, x * BLOCK_SIZE : (x+1) * BLOCK_SIZE] = idct2(apply_mask(blocks[:, :, y, x, 1]) * (MAX_U * BLOCK_SIZE) / MAX_QUANT_UV)
-        v_channel[y * BLOCK_SIZE : (y+1) * BLOCK_SIZE, x * BLOCK_SIZE : (x+1) * BLOCK_SIZE] = idct2(apply_mask(blocks[:, :, y, x, 2]) * (MAX_V * BLOCK_SIZE) / MAX_QUANT_UV)
+        y_channel[y * BLOCK_SIZE : (y+1) * BLOCK_SIZE, x * BLOCK_SIZE : (x+1) * BLOCK_SIZE] = idct2(apply_mask_y(blocks[:, :, y, x, 0]) * (MAX_Y * BLOCK_SIZE) / MAX_QUANT_Y)
+        u_channel[y * BLOCK_SIZE : (y+1) * BLOCK_SIZE, x * BLOCK_SIZE : (x+1) * BLOCK_SIZE] = idct2(apply_mask_uv(blocks[:, :, y, x, 1]) * (MAX_U * BLOCK_SIZE) / MAX_QUANT_UV)
+        v_channel[y * BLOCK_SIZE : (y+1) * BLOCK_SIZE, x * BLOCK_SIZE : (x+1) * BLOCK_SIZE] = idct2(apply_mask_uv(blocks[:, :, y, x, 2]) * (MAX_V * BLOCK_SIZE) / MAX_QUANT_UV)
 
-compression_factor = 8*8*8 / QUANTIZATION_BITS_Y / QUANTIZATION_BITS_UV / QUANTIZATION_BITS_UV * BLOCK_SIZE * BLOCK_SIZE / np.count_nonzero(mask)
+compression_factor = 8 ** 3 / QUANTIZATION_BITS_Y / QUANTIZATION_BITS_UV / QUANTIZATION_BITS_UV * ((BLOCK_SIZE ** 2) ** 3) / (np.count_nonzero(mask_y) * np.count_nonzero(mask_uv) * np.count_nonzero(mask_uv))
+compression_factor_y_only = 8 ** 3 / QUANTIZATION_BITS_Y * ((BLOCK_SIZE ** 2) ** 3) / (np.count_nonzero(mask_y))
 
 plt.figure(figsize=(30, 10))
 
@@ -112,7 +132,7 @@ yuv_data[:, :, 2] = 100
 processed_image = Image.fromarray(yuv_data, "YCbCr").convert("RGB")
 plt.subplot(1, 3, 3)
 plt.imshow(processed_image)
-plt.title(f'DCT Compressed Image [compression factor = {compression_factor * (QUANTIZATION_BITS_Y + QUANTIZATION_BITS_UV) / QUANTIZATION_BITS_Y}]')
+plt.title(f'DCT Compressed Image [compression factor = {compression_factor_y_only}]')
 plt.axis('off')
 plt.axis('tight')
 